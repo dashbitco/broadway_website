cf45ad5c18fd34d8a10d40bc576041facf2ca959
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-05-10 14:57:58 +0200

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./**/*.html', './**/*.css'],
+  darkMode: true,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
