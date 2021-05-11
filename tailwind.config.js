e4ba53cc82ec2797b5ec37a42e3a24bcda515a89
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-05-11 13:16:40 +0200

    Build the leading page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,34 @@ module.exports = {
   purge: ['./**/*.html', './**/*.css'],
   darkMode: true,
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': 'Inter, sans-serif',
+    },
+    extend: {
+      colors: {
+        broadway: {
+          500: '#FF1C1C',
+          600: '#FFDF3F',
+          700: '#FF9901',
+          800: '#29DBFF',
+          900: '#01ADFF',
+        },
+        background: {
+          600: '#41435F',
+          700: '#101228',
+          800: '#070814',
+          900: '#0A0B1C',
+        },
+        gray: {
+          50: '#F8FAFC',
+          200: '#E1E8F0',
+          300: '#CAD5E0',
+          400: '#91A4B7',
+          700: '#304254',
+          800: '#1C2A3A',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
